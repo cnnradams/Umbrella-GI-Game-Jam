797b000b60a0ad7c87fbf00e3956fd57a77e5a5f
Connor Adams <cnnr252@gmail.com>
2019-01-26 13:31:10 -0500

    Buggy platform dropping

--- Assets/Scripts/TestPlayer.cs
+++ Assets/Scripts/TestPlayer.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPlayer : MonoBehaviour {
+public class TestPlayer : MonoBehaviour
+{
 
-	public float moveSpeed = 7;
-	public float jumpForce = 300;
+    public float moveSpeed = 7;
+    public float jumpForce = 300;
 
     private Rigidbody2D rb;
     private List<GameObject> friend_list = new List<GameObject>();
@@ -13,13 +14,17 @@ public class TestPlayer : MonoBehaviour {
     private Animator anim;
 
     private bool canJump;
-
-    void Start () {
-        rb= GetComponent<Rigidbody2D>();
+    private bool dropping;
+    private BoxCollider2D thisCollider;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        thisCollider = GetComponent<BoxCollider2D>();
     }
 
-	void FixedUpdate(){
+    void FixedUpdate()
+    {
 
         float move = Input.GetAxis("Horizontal");
 
@@ -36,14 +41,33 @@ public class TestPlayer : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D Other)
     {
-        if (Other.collider.gameObject.tag == "Ground")
+        Debug.Log(Other.collider.gameObject.tag);
+        if (Other.collider.gameObject.tag == "Ground" || Other.collider.gameObject.tag == "Platform")
         {
             canJump = true;
         }
     }
+    void OnCollisionStay2D(Collision2D Other)
+    {
+        if (Other.collider.gameObject.tag == "Platform")
+        {
+            if (dropping)
+            {
+                Physics2D.IgnoreCollision(Other.collider, GetComponent<Collider2D>());
+                StartCoroutine(Recollide(Other, GetComponent<Collider2D>(), 0.5f));
+            }
+        }
+    }
+    IEnumerator Recollide(Collision2D Other, Collider2D me, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Physics2D.IgnoreCollision(Other.collider, me, false);
+        dropping = false;
+        // Now do your thing here
+    }
     void OnCollisionExit2D(Collision2D Other)
     {
-        if (Other.collider.gameObject.tag == "Ground")
+        if (Other.collider.gameObject.tag == "Ground" || Other.collider.gameObject.tag == "Platform")
         {
             canJump = false;
         }
@@ -57,26 +81,32 @@ public class TestPlayer : MonoBehaviour {
             //Debug.Log("Collided With Friend!");
             GameObject go = collision.gameObject.transform.parent.gameObject;
             Friend friend = go.GetComponent<Friend>();
-            
+
             if (friend.cold)
             {
                 friend_list.Add(go);
                 friend.pickup();
             }
-            
+
         }
     }
 
-    void Update () {
+    void Update()
+    {
 
         // Drop Friends
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (friend_list.Count > 0) {
+            if (friend_list.Count > 0)
+            {
                 Friend friend = friend_list[0].GetComponent<Friend>();
                 friend.drop();
-            friend_list.RemoveAt(0);
+                friend_list.RemoveAt(0);
             }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            dropping = true;
         }
 
         anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
