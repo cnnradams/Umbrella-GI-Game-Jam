03695eea02beac19760a68c3dcd0e7d46ec2ba47
santimuse <santimusella@gmail.com>
2019-01-26 18:11:27 -0500

    Added Warmth-Cold support (for now, in a Slider form), and added a timer.

---
+++ Assets/Scripts/PlayerWarmth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWarmth : MonoBehaviour
+{
+    private float StartingColdDmg = 0.5f;
+    public float StartingPlayerWarmth = 100;
+
+    public float playerWarmth;
+    public float coldDmg;
+
+    public Slider warmthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerWarmth = StartingPlayerWarmth;
+        coldDmg = StartingColdDmg;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerWarmth -= coldDmg * Time.deltaTime;
+        warmthSlider.value = playerWarmth;
+        //Debug.Log(playerWarmth.ToString());
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+
+    public float StartingTimeLeft = 60;
+    private float timeLeft;
+    public Text timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = StartingTimeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        timer.text = timeLeft.ToString("F");
+
+        if (timeLeft <= 0.0f)
+        {
+            timer.text = "GAME OVER";
+        }
+    }
+}
