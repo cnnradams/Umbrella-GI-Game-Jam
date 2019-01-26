84656c077248fdcfcdea8d3bd641a15443c00f84
ddddkim0525 <ddddkim0525@naver.com>
2019-01-26 00:12:25 -0500

    Added Test Player control, Animation, and friend states

---
+++ Assets/Scripts/Friend.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friend : MonoBehaviour
+{
+    public bool cold = true;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cold = true;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Cold", cold);
+    }
+
+    public void pickup()
+    {
+        cold = false;
+        //Change Sprite
+        //Start Following Player
+    }
+    public void drop()
+    {
+        cold = true;
+    }
+}
---
+++ Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+    private Rigidbody2D rb;
+    private List<Friend> friend_list = new List<Friend>();
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        rb= GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+	void FixedUpdate(){
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<Friend>())
+        {
+            Debug.Log("Collided With Friend!");
+            Friend friend = collision.gameObject.GetComponent<Friend>();
+            if (friend.cold)
+            {
+                friend_list.Add(friend);
+                friend.pickup();
+            }
+            
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyDown (KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+		}
+		if(Input.GetKey (KeyCode.D)){
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+		if(Input.GetKey (KeyCode.A)){
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            if(friend_list.Count > 0)
+            friend_list[0].drop();
+            friend_list.RemoveAt(0);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+    }
+}
